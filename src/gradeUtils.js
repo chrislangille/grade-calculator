87e8e52fd76b1d6f0cdd073c72183da94b3b4703
chrislangille <langillechris25@gmail.com>
2024-11-27 12:40:08 -0400

    update syling and npm packages

--- src/gradeUtils.js
+++ src/gradeUtils.js
@@ -1,25 +1,25 @@
 function calculateLetterGrade(overallGrade) {
   if (overallGrade >= 90 && overallGrade <= 100) {
     return "A+";
-  } else if (overallGrade >= 85 && overallGrade <= 89) {
+  } else if (overallGrade >= 85 && overallGrade < 90) {
     return "A";
-  } else if (overallGrade >= 80 && overallGrade <= 84) {
+  } else if (overallGrade >= 80 && overallGrade < 85) {
     return "A-";
-  } else if (overallGrade >= 77 && overallGrade <= 79) {
+  } else if (overallGrade >= 77 && overallGrade < 80) {
     return "B+";
-  } else if (overallGrade >= 73 && overallGrade <= 76) {
+  } else if (overallGrade >= 73 && overallGrade < 77) {
     return "B";
-  } else if (overallGrade >= 70 && overallGrade <= 72) {
+  } else if (overallGrade >= 70 && overallGrade < 73) {
     return "B-";
-  } else if (overallGrade >= 65 && overallGrade <= 69) {
+  } else if (overallGrade >= 65 && overallGrade < 70) {
     return "C+";
-  } else if (overallGrade >= 60 && overallGrade <= 64) {
+  } else if (overallGrade >= 60 && overallGrade < 65) {
     return "C";
-  } else if (overallGrade >= 55 && overallGrade <= 59) {
+  } else if (overallGrade >= 55 && overallGrade < 60) {
     return "C-";
-  } else if (overallGrade >= 50 && overallGrade <= 54) {
+  } else if (overallGrade >= 50 && overallGrade < 55) {
     return "D";
-  } else if (overallGrade >= 0 && overallGrade <= 49) {
+  } else if (overallGrade >= 0 && overallGrade < 50) {
     return "F";
   } else {
     return "N/A";
